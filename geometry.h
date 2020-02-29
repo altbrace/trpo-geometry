cd7583e735610e8f1d5ef5887523e111ed38b1af
altbrace <dim203286@gmail.com>
2020-02-29 16:00:40 +0700

    Types.c removed, figure types defined in geometry.h

---
+++ geometry.h
@@ -0,0 +1,16 @@
+typedef struct Point{
+	double x;
+	double y;
+}Point;
+
+typedef struct Circle{
+	Point center;
+	double radius;
+}Circle;
+
+typedef struct Triangle{
+	Point first;
+	Point second;
+	Point third;
+	Point fourth;
+}Triangle;
