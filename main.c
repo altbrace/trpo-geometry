eb9773d9385566bffd8c09ef6590b76b2094b964
altbrace <dim203286@gmail.com>
2020-02-29 17:11:51 +0700

    Created file main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include "geometry.h"
+
+int main(){
+	char* tri[8] = "triangle";
+	char* cir[6] = "circle";
+	char* input[30];
+	printf("Input WKT-like geometry:\n");
+	scanf("\n%s", )
+}
