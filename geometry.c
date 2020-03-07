4e5523c982b28af9afad4192da44b3cd550ea652
altbrace <dim203286@gmail.com>
2020-03-07 13:12:26 +0700

    Formatted w/ clang-format

--- geometry.c
+++ geometry.c
@@ -1,51 +1,59 @@
-#include <stdio.h>
-#include <math.h>
 #include "geometry.h"
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-double sqr(double x){
-	return pow(x, 2);
+double sqr(double x)
+{
+    return pow(x, 2);
 }
 
-double length(Point first, Point second){
-	double l = sqrt(sqr(second.x-first.x)+sqr(second.y-first.y));
-	return l;
+double length(Point first, Point second)
+{
+    double l = sqrt(sqr(second.x - first.x) + sqr(second.y - first.y));
+    return l;
 }
 
-bool isTriangle(Triangle tri){
-	double ab = length(tri.a, tri.b);
-	double bc = length(tri.b, tri.c);
-	double ac = length(tri.a, tri.c);
-	int x = tri.a.x;
-	int y = tri.a.y;
-	int x1 = tri.check.x;
-	int y1 = tri.check.y;
-	if (ab < bc+ac && bc < ac+ab && ac < ab+bc && x == x1 && y ==y1) return true;
-	return false;
+bool isTriangle(Triangle tri)
+{
+    double ab = length(tri.a, tri.b);
+    double bc = length(tri.b, tri.c);
+    double ac = length(tri.a, tri.c);
+    int x = tri.a.x;
+    int y = tri.a.y;
+    int x1 = tri.check.x;
+    int y1 = tri.check.y;
+    if (ab < bc + ac && bc < ac + ab && ac < ab + bc && x == x1 && y == y1)
+        return true;
+    return false;
 }
 
-double triPeri(Triangle tri){
-	double ab = length(tri.a, tri.b);
-	double bc = length(tri.b, tri.c);
-	double ac = length(tri.a, tri.c);
+double triPeri(Triangle tri)
+{
+    double ab = length(tri.a, tri.b);
+    double bc = length(tri.b, tri.c);
+    double ac = length(tri.a, tri.c);
 
-	return ab+bc+ac;
+    return ab + bc + ac;
 }
 
-double circlePeri(Circle circle){
-	return 2*M_PI*circle.radius;
+double circlePeri(Circle circle)
+{
+    return 2 * M_PI * circle.radius;
 }
 
-double triArea(Triangle tri){
-	double h_peri = triPeri(tri)/2;	
-	double ab = length(tri.a, tri.b);
-	double bc = length(tri.b, tri.c);
-	double ac = length(tri.a, tri.c);
-	double area = sqrt(h_peri*(h_peri-ab)*(h_peri-bc)*(h_peri-ac));
-	return area;
+double triArea(Triangle tri)
+{
+    double h_peri = triPeri(tri) / 2;
+    double ab = length(tri.a, tri.b);
+    double bc = length(tri.b, tri.c);
+    double ac = length(tri.a, tri.c);
+    double area = sqrt(h_peri * (h_peri - ab) * (h_peri - bc) * (h_peri - ac));
+    return area;
 }
 
-double circleArea(Circle circle){
-	return sqr(circle.radius)*M_PI;
+double circleArea(Circle circle)
+{
+    return sqr(circle.radius) * M_PI;
 }
 
--- main.c
+++ main.c
@@ -1,118 +1,128 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <math.h>
-#include <stdlib.h>
 #include "geometry.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char* _input(){
-	char* input = malloc(sizeof(char)*40);
-	printf("Input WKT-like geometry:\n");
-	fgets(input, 40, stdin);
-	return input;
+char* _input()
+{
+    char* input = malloc(sizeof(char) * 40);
+    printf("Input WKT-like geometry:\n");
+    fgets(input, 40, stdin);
+    return input;
 }
 
-int main(){
-	char* input; char input_copy[40];
-	char* parser;
-	char* delim = "() ,";
-	input = _input();
-	strcpy(input_copy, input);
-	parser = strtok(input, delim);
-	if (strcmp(parser, "circle") == 0){
-		Circle circle;
-		parser = strtok(NULL, delim);
+int main()
+{
+    char* input;
+    char input_copy[40];
+    char* parser;
+    char* delim = "() ,";
+    input = _input();
+    strcpy(input_copy, input);
+    parser = strtok(input, delim);
+    if (strcmp(parser, "circle") == 0) {
+        Circle circle;
+        parser = strtok(NULL, delim);
 
-		if (parser != NULL) circle.center.x = atoi(parser);
-		else{
-			printf("Error: invalid circle\n");
-			return -1;
-		}
+        if (parser != NULL)
+            circle.center.x = atoi(parser);
+        else {
+            printf("Error: invalid circle\n");
+            return -1;
+        }
 
-		parser = strtok(NULL, delim);
-		if (parser != NULL) circle.center.y = atoi(parser);
-		else{
-			printf("Error: invalid circle\n");
-			return -1;
-		}
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            circle.center.y = atoi(parser);
+        else {
+            printf("Error: invalid circle\n");
+            return -1;
+        }
 
-		parser = strtok(NULL, delim);
-		if (parser != NULL) circle.radius = atof(parser);	
-		else{
-			printf("Error: invalid circle\n");
-			return -1;
-		}
-		
-		printf("Perimeter: %.2f\n", circlePeri(circle));
-		printf("Area: %.2f\n", circleArea(circle));
-	}
-	else if(strcmp(parser, "triangle") == 0){
-		Triangle tri;
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.a.x = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.a.y = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.b.x = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.b.y = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.c.x = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.c.y = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.check.x = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		parser = strtok(NULL, delim);
-		if (parser != NULL) tri.check.y = atoi(parser);
-		else{
-			printf("Error: invalid triangle\n");
-			return -1;
-		}
-		
-		if (isTriangle(tri) == true){
-			printf("Perimeter: %.2f\n", triPeri(tri));
-			printf("Area: %.2f\n", triArea(tri));
-		}
-		else printf("Error: invalid triangle\n");
-	}
-	else printf("Error: unknown shape '%s'", parser);
-	return 0;
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            circle.radius = atof(parser);
+        else {
+            printf("Error: invalid circle\n");
+            return -1;
+        }
 
+        printf("Perimeter: %.2f\n", circlePeri(circle));
+        printf("Area: %.2f\n", circleArea(circle));
+    } else if (strcmp(parser, "triangle") == 0) {
+        Triangle tri;
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.a.x = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
 
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.a.y = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.b.x = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.b.y = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.c.x = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.c.y = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.check.x = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        parser = strtok(NULL, delim);
+        if (parser != NULL)
+            tri.check.y = atoi(parser);
+        else {
+            printf("Error: invalid triangle\n");
+            return -1;
+        }
+
+        if (isTriangle(tri) == true) {
+            printf("Perimeter: %.2f\n", triPeri(tri));
+            printf("Area: %.2f\n", triArea(tri));
+        } else
+            printf("Error: invalid triangle\n");
+    } else
+        printf("Error: unknown shape '%s'", parser);
+    return 0;
 }
